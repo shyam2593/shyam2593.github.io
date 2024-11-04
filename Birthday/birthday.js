2286b59241ddc5f632952bbe86bb03dbc8e4c30b
Shyam <61116518+shyam2593@users.noreply.github.com>
2024-11-04 14:03:07 +0530

    Update birthday.js

--- Birthday/birthday.js
+++ Birthday/birthday.js
@@ -1,6 +1,6 @@
 const search = window.location.search;
 const names = new URLSearchParams(search).get("n");
-document.getElementById("bname").innerText = names;
+document.getElementById("bname").innerText = atob(names);
 function myTransform() {
   document.getElementsByClassName("card")[0].style.transform =
     "rotateY(180deg)";

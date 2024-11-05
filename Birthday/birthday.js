ffcf9073432e5434e3563234d2aa79280e37f219
Shyam <61116518+shyam2593@users.noreply.github.com>
2024-11-05 10:42:24 +0530

    Update birthday.js

--- Birthday/birthday.js
+++ Birthday/birthday.js
@@ -1,6 +1,14 @@
 const search = window.location.search;
-const names = new URLSearchParams(search).get("n");
-document.getElementById("bname").innerText = atob(names);
+const name = new URLSearchParams(search).get("n");
+if(typeof name === "string"){
+  try{
+    if(name!==""){
+        document.getElementById("bname").innerText = atob(name);
+    }
+  }catch(e){
+    document.getElementById("bname").innerText = name;
+  }
+}
 function myTransform() {
   document.getElementsByClassName("card")[0].style.transform =
     "rotateY(180deg)";
